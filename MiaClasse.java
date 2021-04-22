d12c32fdd3f6f3b665710c0c2703a98927f6ea7b
gristaino <giuseppe.ristaino@soprasteria.com>
2021-04-22 11:39:05 +0200

    modified with errors MiaClass.java

--- MiaClasse.java
+++ MiaClasse.java
@@ -5,11 +5,41 @@ class MiaClasse {
 	String nameClass;
 	String ciccio;
 	
-	public MiaClasse(String nameClass, String ciccio) {
+	public MiaClasse(String nameClass,public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}
+public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}public MiaClasse(String nameClass, String ciccio) {
+		this.ciccio = ciccio;
+		this.nameClass = nameClass;
+	}	String ciccio) {
 		this.ciccio = ciccio;
 		this.nameClass = nameClass;
 	}
 	
+	public String getNome() {
+		return this.nameClass;
+	public String getNome() {
+		return this.nameClass;
+	public String getNome() {
+		return this.nameClass;
+	public String getNome() {
+		return this.nameClass;
 	public String getNome() {
 		return this.nameClass;
 	}
