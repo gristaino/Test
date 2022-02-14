4431b52fec72144b915283753414eaf6b3400b2a
gristaino <giuseppe.ristaino@soprasteria.com>
2022-02-14 12:37:13 +0100

    test cherry pick

--- MiaClasse.java
+++ MiaClasse.java
@@ -9,65 +9,6 @@ class MiaClasse {
 		this.ciccio = ciccio;
 		this.nameClass = nameClass;
 	}
-public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;adsf
-		this.nameClass = nameClass;
-	}public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;
-		this.nameClass = nameClassa
-			d
-			fdsa
-			fad
-			sf
-			ds
-			fad
-			sf
-	}public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;
-		this.nameClass = nameClass;
-	}public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;+
-		Qui iniziano le mie modifiche
-			se è sbagliato qualcosa fammi sapere;
-		this.nameClass = nameClass;
-	}public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;
-		this.nameClass = nameClass;
-	Qua dovrebbero iniziare le tue modifiche
-		metti le tue modifiche qua
-	}public MiaClasse(String nameClass, String ciccio) {
-		this.ciccio = ciccio;
-		this.nameClass = nameClass;
-	}	String ciccio) {
-		Qua scrivo tanto per
-		this.ciccio = ciccio;
-		this.nameClass = nameClass;
-		fdsfdfjkakdfds
-			dasfadsf
-			dsaf
-			dsf
-			dsf
-			d
-			fds
-			fds
-			fd
-			faf
-	}
-	
-	public String getNome() {
-		return this.nameClass;
-	public String getNome() {
-		return this.nameClass;
-	public String getNome() {
-		return this.nameClass;
-	public String getNome() {
-		return this.nameClass;
-	public String getNome() {
-		return this.nameClass;
-	}
-	
-	public String getCiccio() {
-		return ciccio;
-	}
+
 	
 }
