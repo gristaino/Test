4cde7ce68c3966262893423ad2d53c327ec7e143
gristaino <giuseppe.ristaino@soprasteria.com>
2022-03-15 12:29:10 +0100

    test revert 1

--- MiaClasse.java
+++ MiaClasse.java
@@ -8,6 +8,10 @@ class MiaClasse {
 	public MiaClasse(String nameClass,public MiaClasse(String nameClass, String ciccio) {
 		this.ciccio = ciccio;
 		this.nameClass = nameClass;
+		
+		REVERTOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+		
+		
 	}
 public MiaClasse(String nameClass, String ciccio) {
 		this.ciccio = ciccio;adsf
